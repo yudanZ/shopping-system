3a37449240b5abf389713447747f4ee79a4faa27
yudanZ <zhai.yudan@gmail.com>
2020-09-15 10:32:37 +0200

    created react project and added homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HomePage from './homepage.component';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
