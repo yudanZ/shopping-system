d01a5017d39f903fbe2430c6807a8acaade74a50
yudanZ <zhai.yudan@gmail.com>
2020-09-21 21:17:54 +0200

    finished cart drop-down function

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './cart-item.styles.scss';
+
+const CartItem = ({ item: { imageUrl, price, name, quantity }}) => {
+    return <div className='cart-item'>
+                <img src={imageUrl} alt='item'></img>
+                <div className='item-details'>
+                    <span className='name'>{name}</span>
+                    <span className='price'>{quantity} * CHF {price}</span>
+                </div>
+            </div>
+}
+
+export default CartItem;
