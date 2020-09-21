52031895534b097d6e955806b0be4927bf7544be
yudanZ <zhai.yudan@gmail.com>
2020-09-21 20:26:46 +0200

    add quantity to addItems

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -66,49 +66,49 @@ const SHOP_DATA = [
       routeName: 'womens',
       items: [
         {
-          id: 1,
+          id: 10,
           name: 'Knit mini skirt with buckled belt',
           imageUrl: '/images/women/women1.jpg',
           price: 79.90
         },
         {
-          id: 2,
+          id: 11,
           name: 'Short skirt with chain and pleats made of 100% wool',
           imageUrl: '/images/women/women2.jpg',
           price: 89.90
         },
         {
-          id: 3,
+          id: 12,
           name: 'Pleated top',
           imageUrl: '/images/women/women3.jpg',
           price: 49.90
         },
         {
-          id: 4,
+          id: 13,
           name: 'Limited edition shirt with a scarf',
           imageUrl: '/images/women/women4.jpg',
           price: 139.00
         },
         {
-          id: 5,
+          id: 14,
           name: 'Limited edition leather top with bow',
           imageUrl: '/images/women/women5.jpg',
           price: 249.00
         },
         {
-          id: 6,
+          id: 15,
           name: 'High neck sweater with side vents',
           imageUrl: '/images/women/women6.jpg',
           price: 99.90
         },
         {
-          id: 7,
+          id: 16,
           name: 'Limited edition batwing sleeve wool sweater',
           imageUrl: '/images/women/women7.jpg',
           price: 139.00
         },
         {
-          id: 8,
+          id: 17,
           name: 'Wide-neck cape sweater',
           imageUrl: '/images/women/women8.jpg',
           price: 99.90
@@ -121,31 +121,31 @@ const SHOP_DATA = [
       routeName: 'kids',
       items: [
         {
-          id: 1,
+          id: 18,
           name: 'OVERSHIRT WITH POCKETS',
           imageUrl: '/images/kids/kid1.jpg',
           price: 69.90
         },
         {
-          id: 2,
+          id: 19,
           name: 'RIBBED T-SHIRT',
           imageUrl: '/images/kids/kid2.jpg',
           price: 12.90
         },
         {
-          id: 3,
+          id: 20,
           name: 'LAPEL COLLAR COAT',
           imageUrl: '/images/kids/kid3.jpg',
           price: 79.90
         },
         {
-          id: 4,
+          id: 21,
           name: 'VOLUMINOUS JACQUARD DRESS',
           imageUrl: '/images/kids/kid4.jpg',
           price: 39.90
         },
         {
-          id: 5,
+          id: 22,
           name: 'DENIM MIDI SKIRT',
           imageUrl: '/images/kids/kid5.jpg',
           price: 29.90
@@ -158,43 +158,43 @@ const SHOP_DATA = [
       routeName: 'shoes-and-bags',
       items: [
         {
-          id: 1,
+          id: 23,
           name: 'Nappa leather clutch bag with horsebit',
           imageUrl: '/images/bags_shoes/bag1.jpg',
           price: 139.00
         },
         {
-          id: 2,
+          id: 24,
           name: 'Black shoulder leather bag',
           imageUrl: '/images/bags_shoes/bag2.jpg',
           price: 199.00
         },
         {
-          id: 3,
+          id: 25,
           name: 'Heeled animal print leather boots',
           imageUrl: '/images/bags_shoes/bag3.jpg',
           price: 289.00
         },
         {
-          id: 4,
+          id: 26,
           name: 'Brown leather boots with wooden heels',
           imageUrl: '/images/bags_shoes/bag4.jpg',
           price: 249.00
         },
         {
-          id: 5,
+          id: 27,
           name: 'Burgundy horsebit loafers',
           imageUrl: '/images/bags_shoes/bag5.jpg',
           price: 129.00
         },
         {
-          id: 6,
+          id: 28,
           name: 'Cream loafers with horsebit detail',
           imageUrl: '/images/bags_shoes/bag5.jpg',
           price: 129.00
         },
         {
-          id: 7,
+          id: 29,
           name: 'Cream-coloured soft leather ballet flats',
           imageUrl: '/images/bags_shoes/bag5.jpg',
           price: 99.90
@@ -207,37 +207,37 @@ const SHOP_DATA = [
       routeName: 'mens',
       items: [
         {
-          id: 1,
+          id: 30,
           name: 'Wool houndstooth jacket',
           imageUrl: '/images/mens/men1.jpg',
           price: 199.00
         },
         {
-          id: 2,
+          id: 31,
           name: 'Double-faced leather jacket',
           imageUrl: '/images/mens/men2.jpg',
           price: 849.00
         },
         {
-          id: 3,
+          id: 32,
           name: 'Quilted down jacket',
           imageUrl: '/images/mens/men3.jpg',
           price: 169.00
         },
         {
-          id: 4,
+          id: 33,
           name: '100% cotton short sleeve t-shirt',
           imageUrl: '/images/mens/men4.jpg',
           price: 35.90
         },
         {
-          id: 5,
+          id: 34,
           name: 'T-shirt with steve mcqueen photo',
           imageUrl: '/images/mens/men5.jpg',
           price: 35.90
         },
         {
-          id: 6,
+          id: 35,
           name: 'Wool flannel cap',
           imageUrl: '/images/mens/men6.jpg',
           price: 35.90
