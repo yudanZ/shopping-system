64017755fc38a5f3d13de7e55f9a8a59377443b9
yudanZ <zhai.yudan@gmail.com>
2020-09-25 14:00:03 +0200

    change css to be more responsive

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,10 +4,10 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, history, linkUrl , match}) => (
-    <div 
-        style={{ backgroundImage: `url(${imageUrl})`}}
-        className= 'menu-item'
-        onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div className= 'menu-item'>
+        <div className="imageContainer" onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <img src={imageUrl} alt="img"></img>
+        </div>
         <div className="content">
             <h1 className='title'>{ title }</h1>
         </div>
