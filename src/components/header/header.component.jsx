7cf2a30586db15bd9e7cb85aa5afd7fd1bd7a0bf
yudanZ <zhai.yudan@gmail.com>
2020-09-25 21:27:45 +0200

    change header to be more responsive

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import './header.styles.scss';
-
 
 import { Link } from 'react-router-dom';
 
@@ -13,8 +11,53 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
+import './header.styles.scss';
+
+
+
 const Header = ({ currentUser, hidden }) => (
-    
+    <div className="header">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/" style={{color:'#8ebfe7'}}>
+                <i className="fas fa-crown" style={{ width: '30', height: '30'}}></i>
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-md-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to='/shop'>SHOP</Link>
+                    
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to='/contact'>CONTACT</Link>
+                    </li>
+                    {
+                        currentUser ? (
+                            <li className="nav-item">
+                                <a className="nav-link" onClick={() => auth.signOut()} href='#0'>SIGN OUT</a>
+                            </li>
+
+                        ) : (
+                            <li className="nav-item">
+                                <Link to='/signin' className='nav-link'>SIGN IN</Link>
+                            </li>
+                        )
+                    }
+                
+                </ul>
+                <CartIcon></CartIcon>
+            </div>
+            
+        </nav>
+        {   hidden ? null :
+            (<CartDropdown></CartDropdown>)
+
+        }
+    </div>
+    /*
     <div className="header">
         <Link className="logo-container" to="/">
             <i className="fas fa-crown" style={{ fontSize: '48px'}}></i>
@@ -38,7 +81,7 @@ const Header = ({ currentUser, hidden }) => (
 
         }
     </div>
-
+        */
 );
 
 // any time the store is updated, mapStateToProps will be called
